201fd9955a314266ef823bfda583be3390653310
Poignantt <APP@DoodleJump.local>
2016-12-02 01:03:28 +0800

    App Completed

--- E16 Miracle Pill/ViewController.swift
+++ E16 Miracle Pill/ViewController.swift
@@ -12,15 +12,15 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     // weak references mean that the link to the reference will not be held once the view is no longer visible.
     @IBOutlet weak var fullNameTF: UITextField!
-    @IBOutlet weak var fullNameL: UILabel!
+    //@IBOutlet weak var fullNameL: UILabel!
     
     @IBOutlet weak var streetAddressTF: UITextField!
-    @IBOutlet weak var streetAddressL: UILabel!
+    //@IBOutlet weak var streetAddressL: UILabel!
     
     @IBOutlet weak var cityTF: UITextField!
-    @IBOutlet weak var cityL: UILabel!
+    //@IBOutlet weak var cityL: UILabel!
     
-    @IBOutlet weak var stateL: UILabel!
+    //@IBOutlet weak var stateL: UILabel!
     @IBOutlet weak var statePicker: UIPickerView!
     
     @IBOutlet weak var successImage: UIImageView!
@@ -28,21 +28,25 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     @IBOutlet weak var statePickerBTN: UIButton!
     @IBOutlet weak var stateLabel: UILabel!
     
+    
+
     @IBOutlet weak var countryL: UILabel!
     @IBOutlet weak var countryTF: UITextField!
     
-    @IBOutlet weak var zipCodeL: UILabel!
+    
+    @IBOutlet weak var zipcodeL: UILabel!
     @IBOutlet weak var zipeCodeTF: UITextField!
     
-    @IBOutlet weak var buyNowBTN: UIButton!
-    @IBOutlet weak var pillImage: UIImageView!
-    @IBOutlet weak var miracleLabel: UILabel!
-    @IBOutlet weak var costLabel: UILabel!
-    @IBOutlet weak var divider: UIView!
+    //@IBOutlet weak var buyNowBTN: UIButton!
+    //@IBOutlet weak var pillImage: UIImageView!
+    //@IBOutlet weak var miracleLabel: UILabel!
+    //@IBOutlet weak var costLabel: UILabel!
+    //@IBOutlet weak var divider: UIView!
     
     
     var currentRow: Int?
     var states = ["Australian Capital Terrirory","New South Wales","Northern Territory","Queensland","South Australia","Tasmania","Victoria","Western Australia"]
+    var stateAcro = ["ACT","NSW","NT","QLD","SA","TAS","TAS","WA"]
 
 
     override func viewDidLoad() {
@@ -81,23 +85,31 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
             countryL.isHidden = true
             countryTF.isHidden = true
             
-            zipCodeL.isHidden = true
+            zipcodeL.isHidden = true
             zipeCodeTF.isHidden = true
         } else {
         statePicker.isHidden = true
-        stateLabel.text = states[currentRow!]
+            if let row = currentRow {
+              stateLabel.text = states[row]
+            } else {
+              stateLabel.text = states[0]
+            }
         statePickerBTN.setTitle("Change State", for: UIControlState.normal)
             countryL.isHidden = false
             countryTF.isHidden = false
             
-            zipCodeL.isHidden = false
+            zipcodeL.isHidden = false
             zipeCodeTF.isHidden = false
         }
     }
     
 
     @IBAction func buyButtonPress(_ sender: Any) {
-        if fullNameTF.text != "" && streetAddressTF.text != "" && cityTF.text != "" && stateLabel.text != "" && countryTF.text != "" && zipeCodeTF.text != "" {
+        if fullNameTF.text != "" && streetAddressTF.text != "" && cityTF.text != "" && stateLabel.text != "" && countryTF.text != "" && zipeCodeTF.text != "" && statePicker.isHidden == true{
+            for view in self.view.subviews {
+                view.isHidden = true
+            }
+            /*
             fullNameTF.isHidden = true
             fullNameL.isHidden = true
             streetAddressTF.isHidden = true
@@ -109,7 +121,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
             stateL.isHidden = true
             statePicker.isHidden = true
             
-            successImage.isHidden = false
+            
             
             statePickerBTN.isHidden = true
             stateLabel.isHidden = true
@@ -125,7 +137,14 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
             pillImage.isHidden = true
             miracleLabel.isHidden = true
             costLabel.isHidden = true
-            print("Miracle Drug for \(fullNameTF.text)/n @ \(streetAddressTF.text), \(zipeCodeTF.text) \(cityTF.text)/n \(stateLabel.text) \(countryTF.text)")
+            */
+            
+            successImage.isHidden = false
+            
+            var stateAcronym = ""
+            if  currentRow != nil  {stateAcronym = stateAcro[currentRow!]} else {stateAcronym = stateAcro[0]}
+            
+            print("\n\n\nMiracle Drug for\n\(fullNameTF.text!)\n\(streetAddressTF.text!), \(zipeCodeTF.text!) \(cityTF.text!)\n\(stateAcronym), \(countryTF.text!)")
         }
     }
     
